57f381ef1ffcc8a94310088efbb290627167d36f
Breno <onerbreno@outlook.com>
2023-07-17 15:02:56 -0300

    Initial commit

---
+++ src/styles/global.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: ${(props) => props.theme.colors.base.background};
+    color: ${(props) => props.theme.colors.base.text};
+    -webkit-font-smoothing: antialiased; 
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${(props) => props.theme.colors.brand.blue};
+  }
+  
+  body, input, button, textarea {
+    font: ${(props) => props.theme.fonts.text.md}
+  }
+`
---
+++ src/styles/theme/default.ts
@@ -0,0 +1,33 @@
+export const defaultTheme = {
+  colors: {
+    brand: {
+      blue: '#3294F8',
+    },
+    base: {
+      input: '#040F1A',
+      background: '#071422',
+      profile: '#0B1B2B',
+      post: '#112131',
+      border: '#1C2F41',
+      label: '#3A536B',
+      span: '#7B96B2',
+      text: '#AFC2D4',
+      subtitle: '#C4D4E3',
+      title: '#E7EDF4',
+    },
+  },
+  fonts: {
+    title: {
+      lg: '700 normal 1.5rem/1.3 "Nunito", sans-serif',
+      md: '700 normal 1.25rem/1.6 "Nunito", sans-serif',
+      sm: '700 normal 1.125rem/1.6 "Nunito", sans-serif',
+    },
+    text: {
+      md: '400 normal 1rem/1.6 "Nunito", sans-serif',
+      sm: '400 normal 0.875rem/1.6 "Nunito", sans-serif',
+    },
+    component: {
+      link: '700 normal 0.75rem/1.6 "Nunito", sans-serif',
+    },
+  },
+}
