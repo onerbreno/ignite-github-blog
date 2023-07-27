dbf18f60d12d40ff496e3041b1874894618e87d1
Breno <onerbreno@outlook.com>
2023-07-26 22:11:05 -0300

    fix Add href to the GitHub link component

--- src/components/GitHubLink/index.tsx
+++ src/components/GitHubLink/index.tsx
@@ -3,11 +3,12 @@ import { GithubLinkContainer } from './styles'
 
 interface GitHubLinkProps {
   content: string
+  href: string
 }
 
-export function GitHubLink({ content }: GitHubLinkProps) {
+export function GitHubLink({ content, href }: GitHubLinkProps) {
   return (
-    <GithubLinkContainer href="https://github.com/onerbreno" target="_blank">
+    <GithubLinkContainer href={href} target="_blank">
       <span>{content}</span>
       <FaArrowUpRightFromSquare size={12} />
     </GithubLinkContainer>
